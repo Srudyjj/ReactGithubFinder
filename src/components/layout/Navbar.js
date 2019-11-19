a351b41faa57357456c050d66a66d6f028ac975b
Serhii Rudyi <rudyjj@gmail.com>
2019-11-19 22:00:42 +0200

    Project 1:11

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'GitHub Finder',
+    icon: 'fab fa-github'
+  };
+
+  static propTipes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className="navbar bg-primary">
-        <i className="fab fa-github" /> {this.props.title}
+        <i className={this.props.icon} /> {this.props.title}
       </nav>
     );
   }
